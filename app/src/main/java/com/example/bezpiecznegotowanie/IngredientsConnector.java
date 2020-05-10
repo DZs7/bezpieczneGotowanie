cc80ee6c9d36af1bb80c37aac075e9d38a61fe7d
mariusztota <58478622+mariusztota@users.noreply.github.com>
2020-05-10 15:55:29 +0200

    list of ingridians connector

---
+++ app/src/main/java/com/example/bezpiecznegotowanie/IngredientsConnector.java
@@ -0,0 +1,66 @@
+package com.example.chef.utilites_search;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.chef.R;
+
+import java.util.List;
+
+class IngredientsConnector extends RecyclerView.Adapter<IngredientsConnector.ViewHolder> {
+
+
+
+    private List<String> ingredients;
+    private Context context;
+
+    IngredientsConnector(Context context, List<String> ingredients){
+        this.ingredients = ingredients;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public IngredientsConnector.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.ingredients_item, viewGroup, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IngredientsConnector.ViewHolder viewHolder, int i) {
+        viewHolder.bind(ingredients.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredients.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView mIngredient;
+        private ImageButton mRemoveButton;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mIngredient = itemView.findViewById(R.id.ingredient);
+            mRemoveButton = itemView.findViewById(R.id.removeButton);
+            mRemoveButton.setOnClickListener(this);
+        }
+
+        void bind(String currentIngredient){
+            mIngredient.setText(currentIngredient);
+        }
+
+        @Override
+        public void onClick(View v) {
+            ingredients.remove(getAdapterPosition());
+            notifyItemRemoved(getAdapterPosition());
+        }
+    }
+}
