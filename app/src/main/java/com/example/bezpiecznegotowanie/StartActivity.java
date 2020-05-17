b656a695d0c3f706b9280a46bb62d597b6970b54
piotr <piosoboci@gmail.com>
2020-05-17 14:15:34 +0200

    update

---
+++ app/src/main/java/com/example/bezpiecznegotowanie/StartActivity.java
@@ -0,0 +1,11 @@
+package com.example.bezpiecznegotowanie;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.bezpiecznegotowanie.login.LoginController;
+
+public class StartActivity extends AppCompatActivity {
+    
+}
